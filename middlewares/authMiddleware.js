112b8bcefb4e37dc3c517c91f59516efb2baf63a
Anna Cosenza <acarolzita@gmail.com>
2025-04-12 21:49:33 +0100

    !

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,9 +4,13 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
 
 function authenticateToken(req, res, next) {
-  // Get token from header (you can customize how you pass the token)
+  // This line retrieves the value of the Authorization header.
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+  
+  // This line splits the header into two parts: "Bearer" and the actual token.
+  // It expects the header to be in the format "Bearer <token>"
+  const token = authHeader && authHeader.split(" ")[1];
+  
   if (!token) return res.sendStatus(401);
 
   jwt.verify(token, JWT_SECRET, (err, user) => {
@@ -20,3 +24,4 @@ function authenticateToken(req, res, next) {
 }
 
 module.exports = authenticateToken;
+
